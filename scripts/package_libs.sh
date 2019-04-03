2bc9904a3564ef14d232781774c6eac67ccae056
Nathan <nathan@vertile.com>
2019-04-02 20:00:29 -0700

    ensure cuda-8 when packaging libs

--- scripts/package_libs.sh
+++ scripts/package_libs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64
+PATH=/usr/local/cuda-8.0/bin/:$PATH
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 LDDS="$(ldd $DIR/../darknet | grep so | sed -e '/^[^\t]/ d' | sed -e 's/\t//' | sed -e 's/.*=..//' | sed -e 's/ (0.*)//')"
 mkdir -p $DIR/../lib
